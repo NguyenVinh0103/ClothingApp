6014f3b7172c6654b1dc612f34bad752d17811ce
NguyenVinh <vinhnqgcs17528@gmail.com>
2022-12-01 16:26:40 +0700

    Update

---
+++ App.js
@@ -0,0 +1,23 @@
+import {StatusBar} from 'react-native';
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {RootNavigation} from './src/navigation/RootNavigation';
+import { Store } from './src/redux';
+import { Provider } from 'react-redux';
+
+const App = () => {
+  return (
+    <Provider store={Store}>
+      <SafeAreaProvider>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+      <RootNavigation />
+    </SafeAreaProvider>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/navigation/RootNavigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Hello, Home} from '../screens';
+
+const Stack = createStackNavigator();
+export const RootNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Hello"
+          component={Hello}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
